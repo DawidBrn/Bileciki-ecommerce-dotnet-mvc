9ce8b6de7a15ff26c50875f830d1bf7de700fc95
Battlesheep1337 <dawid.baran98@gmail.com>
2022-01-24 12:45:02 +0100

    Dodaj pliki projektów.

---
+++ Bileciki-ecommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bileciki_ecommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
